c803d60bdd18eefa3ae4eaa9f970e27cdca0d351
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2023-02-12 19:55:06 +0900

    ID名の変更とアニメーションの軌跡が消えない問題の修正

--- src/components/easing-editor.vue
+++ src/components/easing-editor.vue
@@ -1,15 +1,14 @@
 <template>
-  <div id="mainDiv">
-    <button @click="resetDemoAnime">a</button>
+  <div id="main">
     <div id="demoAnime">
       <canvas width="235" height="20" ref="demoCanvas"/>
       <div id="demoAnimeBall" ref="demoAnimeBall" :class="{'runDemoAnime':runDemoAnime}"/>
     </div>
-    <div id="cubicEditor">
-      <div>
+    <div id="bezierEditor">
+      <div id="editorButtons">
         <button v-for="buttonBezier in buttonsBezier" :key="buttonBezier.key" @click="useTemplate(buttonBezier)">
           <svg
-            width="50" height="50" viewBox="-10 -10 120 120" xmlns="http://www.w3.org/2000/svg" class="buttonBezier"
+            width="50" height="50" viewBox="-10 -10 120 120" xmlns="http://www.w3.org/2000/svg"
           >
             <path :d="`M 0 100 L ${buttonBezier[0]*100} ${(1-buttonBezier[1])*100}`" stroke="#6B6B6B" stroke-width="3"/>
             <path :d="`M 100 0 L ${buttonBezier[2]*100} ${(1-buttonBezier[3])*100}`" stroke="#6B6B6B" stroke-width="4"/>
@@ -57,6 +56,7 @@ export default {
   emits: ["update:modelValue"],
   data(){return {
     cubicBezier: [0.4, 0.1, 0.4, 0.8],
+    cubicBezierChanged: false,
     inputSize: 165,
     inputWidth: 165,
     inputHeight: 180,
@@ -69,7 +69,8 @@ export default {
       [0, 0, 0.6, 1]
     ],
     runDemoAnime: false,
-    animationTime: "1000ms"
+    animationTime: "1000ms",
+    drawFrame: 20
   }},
   mounted(){
     let newCubicBezier = this.modelValue.replace("cubic-bezier(", "").replace(")", "").split(", ")
@@ -82,6 +83,8 @@ export default {
 
     this.demoAnimeCanvas = this.$refs["demoCanvas"]
     this.animeCTX = this.demoAnimeCanvas.getContext("2d")
+
+    this.startDemoAnime()
 
     addEventListener("mousemove", this.movePoint)
     addEventListener("mouseup", this.endMove)
@@ -108,9 +111,11 @@ export default {
         }
       }
     },
+
     getMousePos(mousePos, elementPos){
       return Math.round(((mousePos - elementPos) - (((this.svgSize-100)/2) / this.inputSize * this.svgSize)) / this.inputSize * this.svgSize) / 100
     },
+
     startMove(e){
       this.mouseDown = true
       const mouseLeft = this.getMousePos(e.clientX, this.$refs.mainView.getBoundingClientRect().left)
@@ -123,26 +128,33 @@ export default {
 
     endMove(){
       this.mouseDown = false
-      this.startDemoAnime()
+      if(this.cubicBezierChanged){
+        this.startDemoAnime()
+        this.cubicBezierChanged = false
+      }
     },
 
     startDemoAnime(){
+      this.resetDemoAnime()
       this.runDemoAnime = true
+      this.animeCTX.fillStyle = "#9b26b020"
+      const drawInterval = parseInt(this.animationTime, 10)/this.drawFrame
       const ball = this.$refs["demoAnimeBall"]
       const canvasLeft = this.demoAnimeCanvas.getBoundingClientRect().left
+
       let time = 0
-      const drawFPS = parseInt(this.animationTime, 10)/20
-      this.animeCTX.fillStyle = "#9b26b00A"
       const drawBall = setInterval(()=>{
         const ballLeft = ball.getBoundingClientRect().left
-        const ballCanvasLeft = ballLeft - canvasLeft
-        console.log(ballCanvasLeft);
-        this.animeCTX.arc(ballCanvasLeft, 10, 10, 0, Math.PI * 2, true);
+        this.animeCTX.beginPath()
+        this.animeCTX.arc(ballLeft-canvasLeft, 10, 10, 0, Math.PI * 2, true);
         this.animeCTX.fill()
-
-        if(1 <= time){clearInterval(drawBall)}
-        time += drawFPS/1000
-      }, drawFPS)
+        if(1 <= time){
+          clearInterval(drawBall)
+          this.runDemoAnime = false
+        }else{
+          time += 1/this.drawFrame
+        }
+      }, drawInterval)
     },
 
     resetDemoAnime(){
@@ -152,8 +164,6 @@ export default {
 
     useTemplate(buttonBezier){
       this.cubicBezier = buttonBezier.concat()
-      this.resetDemoAnime()
-      setTimeout(this.startDemoAnime, 30)
     }
   },
   watch: {
@@ -169,18 +179,14 @@ export default {
         })
         newCubicBezier += ")"
         this.$emit("update:modelValue", newCubicBezier)
+        this.cubicBezierChanged = true
       }
     }
   }
 }
 </script>
 <style scoped>
-#cubicOutput {
-  font-size: 12px;
-  text-align: center;
-}
-
-#mainDiv {
+#main {
   display: flex;
   flex-direction: column;
   gap: 40px;
@@ -188,26 +194,6 @@ export default {
   width: 275px;
   padding: 20px;
   border-radius: 6px;
-}
-
-#cubicEditor {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-  margin: 0 auto 0 auto;
-}
-
-#cubicEditor div {
-  gap: 15px;
-  display: flex;
-  flex-direction: column;
-}
-
-
-.buttonBezier {
-  background-color:#f5f5f5;
-  padding: 6px;
-  margin: 0 0 0 0;
 }
 
 #demoAnime {
@@ -222,10 +208,37 @@ export default {
   background: #9C26B0;
   border-radius: 50%;
   position: absolute;
+  opacity: 0;
 }
 
 .runDemoAnime {
   transition: left v-bind(animationTime) v-bind(modelValue);
   left: 215px !important;
+  opacity: 100% !important;
+}
+
+#bezierEditor {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  margin: 0 auto 0 auto;
+}
+
+#editorButtons {
+  gap: 15px;
+  display: flex;
+  flex-direction: column;
+}
+
+
+#editorButtons button svg {
+  background-color:#f5f5f5;
+  padding: 6px;
+  margin: 0 0 0 0;
+}
+
+#cubicOutput {
+  font-size: 12px;
+  text-align: center;
 }
 </style>
